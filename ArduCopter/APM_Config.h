6a843e90b7afcf09ab26d8352cecf9a2c60a5eed
Jolyon Saunders <futurejman@gmail.com>
2015-06-22 17:12:29 +1000

    Changes to config file for this branch only

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -1,7 +1,8 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 // User specific config file.  Any items listed in config.h can be overridden here.
-
+#define COMPOUND ENABLED
+#define AIRSPEED ENABLED
 // If you used to define your CONFIG_APM_HARDWARE setting here, it is no longer
 // valid! You should switch to using a HAL_BOARD flag in your local config.mk.
 
